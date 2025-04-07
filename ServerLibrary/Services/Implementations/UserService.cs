6caa6efa2c7f1f72c3060a9dcd2337866f04dfb0
b_tursunov <b_tursunov@01-B-TUSUNOV>
2025-04-07 09:33:32 +0500

    добавил хэширование пароля, и переподключил бд в облачную, чтобы данные были синхронизированы в обоих компах

--- ServerLibrary/Services/Implementations/UserService.cs
+++ ServerLibrary/Services/Implementations/UserService.cs
@@ -1,5 +1,4 @@
 ﻿using BaseLibrary.Entities;
-using BaseLibrary.Entities.Auth;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using ServerLibrary.Data;
@@ -37,11 +36,8 @@ namespace ServerLibrary.Services.Implementations
                     throw new InvalidOperationException("Пользователь с таким Email уже существует.");
                 }
 
-                var user = await _repository.CreateAsync(new Register()
-                {
-                    Username = user.,
-                    //TODO : добавить хеширование пароля
-                });
+                entity.PasswordHash = BCrypt.Net.BCrypt.HashPassword(entity.PasswordHash);
+                await _repository.CreateAsync(entity);
 
                 _logger.LogInformation($"Пользователь {entity.Username} успешно добавлен в базу данных");
                 return entity;
