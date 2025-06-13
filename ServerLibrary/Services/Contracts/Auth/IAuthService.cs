b5e5c3aac7729aad79b9cbd66c531c47d13a2640
Bakha <63169312+BahodurTursunov@users.noreply.github.com>
2025-06-13 17:04:03 +0500

    delete generate, verify and userprofile methods

--- ServerLibrary/Services/Contracts/Auth/IAuthService.cs
+++ ServerLibrary/Services/Contracts/Auth/IAuthService.cs
@@ -4,11 +4,8 @@ namespace ServerLibrary.Services.Contracts.Auth
 {
     public interface IAuthService
     {
-        string Generate(string password);
-        bool Verify(string password, string passwordHash);
         Task<UserDTO> RegisterAsync(RegisterUserDTO dto, CancellationToken cancellationToken);
         Task<AuthResponseDTO> LoginAsync(LoginUserDTO dto, CancellationToken cancellationToken);
-        Task<UserDTO> GetUserProfileAsync(int userId, CancellationToken cancellationToken);
         Task<AuthResponseDTO> RefreshTokenAsync(RefreshTokenDTO dto, CancellationToken cancellationToken);
     }
 }
