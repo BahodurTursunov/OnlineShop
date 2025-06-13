96bb39025c5dfdec4c3f0c9d285424b7c75dedec
Bakha <63169312+BahodurTursunov@users.noreply.github.com>
2025-06-13 16:59:59 +0500

    change annotation

--- BaseLibrary/Entities/Category.cs
+++ BaseLibrary/Entities/Category.cs
@@ -6,7 +6,6 @@ namespace BaseLibrary.Entities
     public class Category : BaseEntity
     {
         [Required(ErrorMessage = "Название категории обязательно к заполнению")]
-        [MaxLength(100, ErrorMessage = "Название не должно превышать 100 ")]
         public string Name { get; set; } = "";
         //public int ChildCategoryId { get; set; }
 
